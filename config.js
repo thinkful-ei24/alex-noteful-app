d22231d9b86b18639cb7d4a0af473152289a84a8
Alex <0alexgutierrez@gmail.com>
2018-09-24 09:01:42 -0700

    npm install

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 module.exports = {
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 8081,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/noteful-test'
 };
